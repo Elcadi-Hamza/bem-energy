62773867b6b4775e0d0b423695fdfc5ef796e535
188257464+Elcadi-Hamza@users.noreply.github.com <hamza.elcadi-etu@etu.univh2c.ma>
2025-04-17 17:19:52 +0100

    routings and session

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,9 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\clientController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
 
 Route::get('/dashboard', function () {
     return view('dashboard');
@@ -18,3 +17,53 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__.'/auth.php';
+Route::get('/welcome', function () {
+    return view('welcome');
+});
+
+Route::get('/',function () {
+    return view('frontEnd.app');
+});
+
+Route::get('/services',function () {
+    return view('frontEnd.service');
+});
+
+Route::get('/produits',function () {
+    return view('frontEnd.produit');
+});
+
+Route::get('/about',function () {
+    return view('frontEnd.about');
+});
+
+Route::get('/blogs',function () {
+    return view('frontEnd.blog');
+});
+
+Route::get('/contact',function () {
+    return view('frontEnd.contact');
+});
+
+Route::get('/panier',function () {
+    return view('frontEnd.cart');
+});
+
+Route::get('/login',function () {
+    return view('frontEnd.login');
+})->name('login');
+Route::get('/sing', function () {
+    $sing = true;
+    return view('frontEnd.login', compact('sing'));
+});
+
+
+Route::post('/verification',[clientController::class, 'store'])->name('ajouterClient');
+Route::get('/valid', function () {
+    return view('frontEnd.validation');
+});
+Route::post('/verify-code', [clientController::class, 'verifyCode'])->name('verifyClient');
+
+Route::get('/verify-email/{token}', function ($token) {
+    return "Verification token: " . $token;
+})->name('verify.email');
