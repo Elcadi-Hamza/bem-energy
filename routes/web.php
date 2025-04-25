908a431fc9a27b7b3d0f695939519100fa1451ce
yassine bahadi <yassinebahadi04@gmail.com>
2025-04-25 12:51:41 +0000

    LOgout fix

--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,11 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+Route::post('/logout', function () {
+    Auth::logout();
+    return redirect('/');
+})->name('logout');
+
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
